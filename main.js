e79c9783586acdbdebb82221167783eb5073d834
peterrobert <pwambui93@gmail.com>
2020-02-20 12:53:13 +0300

    project completed

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
